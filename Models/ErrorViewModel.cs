56118c0e8192aabcf722f22989b0632cd1b9c3ba
Manojnayak2200@outlook.com <Manojnayak2200@outlook.com>
2023-02-01 15:24:38 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StudentAdmissionService.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
